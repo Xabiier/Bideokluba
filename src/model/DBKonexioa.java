c4498b90559995dea5b7d1b39f19d98f84b52362
Eneko <enekosampedro@gmail.com>
2016-04-04 16:20:05 +0200

    Kontsultak eta aldaketak egiteko metodoak. Kode garbiketa

---
+++ src/model/DBKonexioa.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DBKonexioa {
+	
+	private Connection konexioa = null;
+
+    private String erabiltzaileaDB = "root";
+    
+    private String pasahitzaDB = ""; 
+
+    private String hostDB = "localhost";
+
+    private String izenaBD = "Bideokluba";
+	
+	public void konektatu() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String datuBasea = "jdbc:mysql://"+hostDB+"/"+izenaBD;
+            konexioa = DriverManager.getConnection(datuBasea, erabiltzaileaDB, pasahitzaDB);
+            if(this.konexioa != null){
+                System.out.println("Konektatu da");
+            }else{
+                System.out.println("Ez da konektatu");                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public ResultSet kontsultatu(String pKontsulta) {
+		ResultSet emaitza;
+		try {
+			Statement kontsulta = this.konexioa.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			emaitza = kontsulta.executeQuery(pKontsulta);
+		} catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+		}
+		return emaitza;
+	}
+	
+	public boolean aldatu(String pAldaketa) {
+		try {
+			Statement aldaketa =  this.konexioa.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+            aldaketa.executeUpdate(pAldaketa);
+            aldaketa.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+}
