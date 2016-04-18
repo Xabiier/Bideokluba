941f44eb0505e142069597ccf75bf029d412a1e5
Iker <ikerzalla@gmail.com>
2016-04-18 18:00:46 +0200

    Administratzaileak bazkide bati alta/baja ematea eta pelikula bat bideoklubean sartzea eginda (kontsolan).
    P.D.: Bazkide bati baja ematearen kasuan baliteke pelikulak kendu behar
    izatea

--- src/model/Bideokluba.java
+++ src/model/Bideokluba.java
@@ -56,27 +56,24 @@ public class Bideokluba {
 						+"FROM `BAZKIDE` "
 						+"WHERE `Kodea`='" + pErabiltzailea + "' and `Pasahitza`='"+ pPasahitza + "';";
 		ResultSet rs = con.kontsultatu(query);
-		boolean admin = false;
 		try {
 			if(!rs.isBeforeFirst()){
 				System.out.println("Ez da erabiltzaile hori existitzen");
 			}
 			else{
 				rs.next();
-				if(rs.getInt(9) == 1){
-					admin = true;
-				}
 				System.out.println("Ongi etorri, " + rs.getString(3));
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	public void administratzaileaKonektatu() {
-		System.out.println("Administratzaile bezala sartu zara\n");
-		System.out.println("Zer egin nahiko zenuke?\n"
+		
+		System.out.println("\nAdministratzaile menua:\n");
+		
+		System.out.println("\nZer egin nahiko zenuke?\n"
 				+ "1 - Bazkide berria sartu\n"
 				+ "2 - Bazkide bati alta/baja eman\n"
 				+ "3 - Pelikula berria sartu\n"
@@ -95,26 +92,26 @@ public class Bideokluba {
 			break;
 		}
 		case(2):{
-			
+			bazkideAltaBaja();
 			break;
 		}
 		case(3):{
-				
+			pelikulaGehitu();
 			break;
 		}
 		case(4):{
-			
+			pelikulaBaja();
 			break;
 		}
 		case(5):{
-			System.out.println();
+			System.out.println("Agur");
 		
 			break;
 		}
 		}
 		
 	}
-	
+
 	private void bazkideGehitu(){
 		
 		String query;
@@ -145,10 +142,87 @@ public class Bideokluba {
 		data = df.format(cal.getTime()); //Gaurko data jarriko da
 		
 		query = "INSERT INTO Bazkide"
-		+ "(`Kodea`, `Pasahitza`, `Izena`, `Abizena`, `Helbidea`, `Kreditua`, `Egoera`, `Noiztik` "
-		+ "VALUES('"+ kodea +"', '"+ pasahitza +"', '"+ izena +"', '"+ abizena +"', '"+ helbidea +"', "+ kreditua +", '"+ egoera +"', '"+ data +"'";
+			  + "(`Kodea`, `Pasahitza`, `Izena`, `Abizena`, `Helbidea`, `Kreditua`, `Egoera`, `Noiztik` "
+		      + "VALUES('"+ kodea +"', '"+ pasahitza +"', '"+ izena +"', '"+ abizena +"', '"+ helbidea +"', "+ kreditua +", '"+ egoera +"', '"+ data +"'";
 		
 		con.aldatu(query);
+	}
+
+	private void bazkideAltaBaja() {
+		String query;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Zein bazkideri nahi diozu alta/baja eman? Idatzi haren kodea: ");
+		String kodea = sc.next();
+		
+		query =	 "SELECT *"
+				+"FROM `BAZKIDE`"
+				+"WHERE `Kodea`='" + kodea + "';";
+		
+		ResultSet rs = con.kontsultatu(query);
+		try {
+			if(!rs.isBeforeFirst()){
+				System.out.println("Ez da erabiltzaile hori existitzen");
+			}
+			else{
+				String aukera;
+				rs.next();
+				if(rs.getString(7).equalsIgnoreCase("baja")){
+					System.out.println("Erabiltzaile hori bajan dago, alta eman nahi diozu? (B/E) ");
+					aukera = sc.next();
+					if(aukera.equalsIgnoreCase("B")){
+						query = "ALTER TABLE `BAZKIDE`"
+							  + "ADD COLUMN `egoera`"
+							  + "'alta'";
+					}
+				}
+				else{
+					System.out.println("Erabiltzaile hori altan dago, baja eman nahi diozu? (B/E) ");
+					aukera = sc.next();
+					if(aukera.equalsIgnoreCase("B")){
+						query = "ALTER TABLE `BAZKIDE`"
+							  + "ADD COLUMN `egoera`"
+							  + "'baja'";
+						
+					//GARRANTZITSUA, BAZKIDE HORI ALOKATUTA DAUZKAN PELIKULAK ITZULI BEHAR AL DIRA?
+					}
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void pelikulaGehitu() {
+		String query;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Sartu pelikularen kodea: ");
+		String kodea = sc.next();
+		
+		System.out.print("Sartu pelikualaren izena: ");
+		String izena = sc.next();
+		
+		System.out.print("Sartu pelikularen prezioa: ");
+		Integer prezioa = Integer.parseInt(sc.next());
+		
+		String egoera = "alta"; //alta edo baja
+		
+		String data;
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar cal = Calendar.getInstance();
+		data = df.format(cal.getTime()); //Gaurko data jarriko da
+		
+		query = "INSERT INTO `BAZKIDE`"
+			  + "(`Kodea`, `Izena`, `Prezioa`, `Egoera`, `Sartze_data` "
+		      + "VALUES('"+ kodea +"', '"+ izena +" ', "+ prezioa +", '"+ egoera +"', '"+ data +"'";
+		
+		con.aldatu(query);
+	}
+	
+	private void pelikulaBaja() {
+		// TODO Auto-generated method stub
+		
 	}
 
 
